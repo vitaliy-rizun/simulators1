1f6fd05c1e884c49892e1148f825e2626b974efa
vitaliy-rizun <betaxa4uvak@rambler.ru>
2022-06-17 13:10:47 +0300

    Add files via upload

---
+++ habahahhaba2/components/company_info_detail.php
@@ -0,0 +1,53 @@
+<section class="intro-alt">
+  <div class="container">
+    <div class="intro-text">
+      <h1 class="intro-title">Скидка 50%</h1>
+      <p class="intro-description">На жёлтый двухместный диван</p>
+      <p class="intro-info">По кодовому слову<code>bmoopk</code></p>
+    </div>
+    <div class="intro-item">
+      <img src="img/hero-image-bmoopk.png" width="272" height="120" alt="Двухместный диван «Бмоопк»">
+      <p><b>Бмоопк</b>, двухместный диван</p>
+      <span class="intro-price"><del>2999₽</del> 1498₽</span>
+    </div>
+  </div>
+</section>
+
+<section class="services">
+  <div class="container">
+    <h2 class="services-title">Наши услуги</h2>
+    <ul class="services-list">
+      <li class="service-cleaning">Очистим квартиру от вашей старой мебели</li>
+      <li class="service-placing">Расставим нашу мебель, красивую и новую</li>
+      <li class="service-decorating">Подарим кустик фикуса для большей красоты</li>
+    </ul>
+  </div>
+</section>
+
+<section class="popular-products">
+  <div class="container">
+    <h2 class="products-title">Самые популярные товары</h2>
+    <ul class="products-list">
+      <li>
+        <a class="product-card product-card-wide" href="catalog-item.html">
+          <div>
+            <h3>Мсеюлида</h3>
+            <p>лампа напольная</p>
+            <span class="price"><del>289₽</del> 288₽</span>
+          </div>
+          <img src="img/item-mseyulida-small.jpg" width="100" height="111" alt="Напольная лампа «Мсеюлида»">
+        </a>
+      </li>
+      <li>
+        <a class="product-card product-card-wide" href="catalog-item.html">
+          <div>
+            <h3>Блетуб</h3>
+            <p>люстра потолочная</p>
+            <span class="price">1360₽</span>
+          </div>
+          <img src="img/item-bletub-small.jpg" width="100" height="111" alt="Потолочная люстра «Блетуб»">
+        </a>
+      </li>
+    </ul>
+  </div>
+</section>
---
+++ habahahhaba2/index.php
@@ -0,0 +1,7 @@
+<?php
+require('components/header.php');
+
+// Раскомментируйте строку ниже
+require('components/company_info_detail.php');
+require('components/footer.php');
+?>
